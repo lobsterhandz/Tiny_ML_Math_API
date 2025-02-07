99d682351f1745f372200e9bf13bdaa730d48323
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2025-02-07 17:10:06 -0500

    Added Flask-CORS support to fix frontend API access

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.exceptions import HTTPException
 import os
-
+from flask_cors import CORS
 from database.models import db, MathOperation
 from ml.ml_model import TinyMLMath  # Import TinyMLMath model
 from config import CurrentConfig
